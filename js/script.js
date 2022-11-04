39705cff0b80c5f0a681f96861af84d59b83868a
LucaBonifazio <luca.bonifazio13@gmail.com>
2022-11-04 16:02:42 +0100

    first step

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
 // Descrizione:
 // Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
+const boxNumber = document.querySelector('.counter-box');
+
+const arrRandoms = [];
+
+for (let i = 1; i <= 5; i++) {
+    getRandomInteger = Math.floor(Math.random() * 100 ) + 1;
+    boxNumber.append(getRandomInteger);
+}
+
+function getRandomInteger(min, max) {
+	return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
 // Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 // Consigli del giorno:
