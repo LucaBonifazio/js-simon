9d72d74360383d4e8f47c277315ebfe7eacfa128
LucaBonifazio <luca.bonifazio13@gmail.com>
2022-11-05 15:54:06 +0100

    Take more time for solution

--- js/script.js
+++ js/script.js
@@ -1,17 +1,18 @@
-// Descrizione:
-// Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
-const boxNumber = document.querySelector('.counter-box');
-const boxResult = document.querySelector('.counter-box-result');
 
-function getRandomInteger(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) ) + min;
+const boxNumber = document.querySelector('.counter-box');
+const boxResult = document.querySelector('.result');
+const boxTimer = document.querySelector('.timer');
+
+const idInterval = setInterval(userMemory, 3000);
+setInterval(stopInterval, 3000);
+
+function userMemory() {
+    prompt("Dimmi il numero");
 }
 
-const eleMemory = setTimeout(getRandomInteger, 5000);
-
-function getRandomInteger() {
-    document.getElementsByClassName("game-box-result").innerHTML = "Ripeti i numeri";
-  }
+function stopInterval() {
+    clearInterval(idInterval);
+}
 
 for (let i = 1; i <= 5; i++) {
     getRandomInteger = Math.floor(Math.random() * 100 ) + 1;
@@ -23,9 +24,4 @@ for (let i = 1; i <= 5; i++) {
 
 
 
-// Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
-// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
-// Consigli del giorno:
-// * Pensate prima in italiano.
-// * Dividete in piccoli problemi la consegna.
-// * Individuate gli elementi di cui avete bisogno per realizzare il programma.
+
